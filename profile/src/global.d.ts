6635125c45a82b032b6e4d226c36d7b3cff63ada
Vani Limbagan <vani.limbagan@sansan.com>
2026-04-20 22:20:57 +0800

    feat(base): profile base app

--- profile/src/global.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import '@react-three/fiber';
-
-declare module '@react-three/fiber' {
-  interface ThreeElements {
-    bentPlaneGeometry: any;
-  }
-}
